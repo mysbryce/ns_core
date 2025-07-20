6c797b20a1a379029dfcd577990369bbf63a7c13
999s <123440999+mysbryce@users.noreply.github.com>
2025-07-20 21:56:30 +0700

    feat(lib/server): add a event routes management

--- lib/server.lua
+++ lib/server.lua
@@ -1,3 +1,4 @@
+local appData = { events = {} }
 --- @diagnostic disable-next-line: lowercase-global
 lib = lib or {}
 
@@ -5,14 +6,42 @@ lib.server = {}
 
 assertType = exports.ns_core:assertType
 
+--- @return NS.Server
 function lib.server:new()
   local data = {}
 
   setmetatable(data, self)
 
+  self.__index = self
+
   return self
 end
 
-function lib.server:use(name, mounter)
-  
+lib.server.event = {}
+
+--- @param name string
+--- @param handler fun(...)
+--- @param isNet boolean?
+function lib.server.event:register(name, handler, isNet)
+  assertType(1, name, 'string')
+
+  if isNet == nil then isNet = false end
+
+  if isNet then
+    RegisterNetEvent(name)
+  end
+
+  return AddEventHandler(name)
+end
+
+--- @param mounted string
+function lib.server.event:use(mounted)
+  assertType(1, mounted, 'string')
+
+  --- @param name string
+  --- @param handler fun(...)
+  --- @param isNet boolean?
+  return function(name, handler, isNet)
+    return self:register(('%s:%s'):format(mounted, name), handler, isNet)
+  end
 end
