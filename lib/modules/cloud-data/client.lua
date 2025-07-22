8326393d602265b28c9bf43bd2debd50dcce38f9
999s <123440999+mysbryce@users.noreply.github.com>
2025-07-23 06:38:15 +0700

    perf(cloud-data): improve performance

--- lib/modules/cloud-data/client.lua
+++ lib/modules/cloud-data/client.lua
@@ -1,27 +1,70 @@
 --- @diagnostic disable-next-line: lowercase-global
 lib = lib or {}
 
-lib.cloud_data = { watchers = {}, data = {} }
+lib.cloud_data = {
+  watchers = {},
+  data = {},
+  watcherCount = {},
+  requestedProps = {}
+}
 
 --- @param serverProp string
 --- @param cb fun(newValue: any)
---- @return integer
+--- @return integer watcherId
 function lib.cloud_data.watch(serverProp, cb)
-  if lib.cloud_data.watchers[serverProp] == nil then
-    lib.cloud_data.watchers[serverProp] = {}
+  if type(serverProp) ~= 'string' or type(cb) ~= 'function' then
+    error('Invalid parameters: serverProp must be string, cb must be function')
   end
 
-  local nums = #lib.cloud_data.watchers[serverProp]
-  local newIndex = nums + 1
+  if lib.cloud_data.watchers[serverProp] == nil then
+    lib.cloud_data.watchers[serverProp] = {}
+    lib.cloud_data.watcherCount[serverProp] = 0
+  end
 
-  lib.cloud_data.watchers[serverProp][newIndex] = cb
-  lib.cloud_data.data[serverProp] = nil
+  local watcherId = lib.cloud_data.watcherCount[serverProp] + 1
+  lib.cloud_data.watcherCount[serverProp] = watcherId
 
-  pcall(function()
+  lib.cloud_data.watchers[serverProp][watcherId] = cb
+
+  if not lib.cloud_data.requestedProps[serverProp] then
+    lib.cloud_data.requestedProps[serverProp] = true
+    
     TriggerServerEvent('ns_core.cloud-data:watch', serverProp)
-  end)
+    
+    if lib.cloud_data.data[serverProp] ~= nil then
+      pcall(cb, lib.cloud_data.data[serverProp])
+    end
+  else
+    if lib.cloud_data.data[serverProp] ~= nil then
+      pcall(cb, lib.cloud_data.data[serverProp])
+    end
+  end
 
-  return newIndex
+  return watcherId
+end
+
+--- @param serverProp string
+--- @param watcherId integer
+--- @return boolean success
+function lib.cloud_data.unwatch(serverProp, watcherId)
+  if lib.cloud_data.watchers[serverProp] and lib.cloud_data.watchers[serverProp][watcherId] then
+    lib.cloud_data.watchers[serverProp][watcherId] = nil
+    
+    local hasWatchers = false
+    for _ in pairs(lib.cloud_data.watchers[serverProp]) do
+      hasWatchers = true
+      break
+    end
+    
+    if not hasWatchers then
+      lib.cloud_data.watchers[serverProp] = nil
+      lib.cloud_data.requestedProps[serverProp] = nil
+      TriggerServerEvent('ns_core.cloud-data:unwatch', serverProp)
+    end
+    
+    return true
+  end
+  return false
 end
 
 --- @generic T
@@ -37,8 +80,26 @@ RegisterNetEvent('ns_core.cloud-data:update', function(serverProp, newValue)
   if lib.cloud_data.watchers[serverProp] ~= nil then
     lib.cloud_data.data[serverProp] = newValue
 
-    pcall(function()
-      lib.cloud_data.watchers[serverProp](newValue)
-    end)
+    for _, cb in pairs(lib.cloud_data.watchers[serverProp]) do
+      pcall(cb, newValue)
+    end
+  end
+end)
+
+RegisterNetEvent('ns_core.cloud-data:initial', function(serverProp, initialValue)
+  lib.cloud_data.data[serverProp] = initialValue
+  
+  if lib.cloud_data.watchers[serverProp] then
+    for _, cb in pairs(lib.cloud_data.watchers[serverProp]) do
+      pcall(cb, initialValue)
+    end
+  end
+end)
+
+AddEventHandler('onResourceStop', function(resourceName)
+  if resourceName == GetCurrentResourceName() then
+    for prop in pairs(lib.cloud_data.requestedProps) do
+      TriggerServerEvent('ns_core.cloud-data:unwatch', prop)
+    end
   end
 end)
--- lib/modules/cloud-data/server.lua
+++ lib/modules/cloud-data/server.lua
@@ -1,44 +1,176 @@
 --- @diagnostic disable-next-line: lowercase-global
 lib = lib or {}
 
-lib.cloud_data = { watchers = {}, data = {} }
+lib.cloud_data = {
+  watchers = {},
+  data = {},
+  validProps = {}
+}
 
 --- @generic T
 --- @param prop string
 --- @param initialValue T 
+--- @return function getter, function setter
 function lib.cloud_data.create(prop, initialValue)
-  lib.cloud_data.data[prop] = initialValue
+  if type(prop) ~= 'string' then
+    error('Property name must be a string')
+  end
 
-  return
-    --- @generic T
-    --- @return T
-    function()
-      return lib.cloud_data.data[prop]
-    end,
-    --- @generic T
-    --- @param newValue T
-    function(newValue)
+  lib.cloud_data.data[prop] = initialValue
+  lib.cloud_data.validProps[prop] = true
+
+  local getter = function()
+    return lib.cloud_data.data[prop]
+  end
+
+  local setter = function(newValue)
+    if lib.cloud_data.data[prop] ~= newValue then
       lib.cloud_data.data[prop] = newValue
 
       if lib.cloud_data.watchers[prop] ~= nil then
-        pcall(function()
-          for _, playerId in ipairs(lib.cloud_data.watchers[prop]) do
-            TriggerClientEvent('ns_core.cloud-data:watch', playerId, prop, newValue)
+        local watchersList = {}
+        
+        for i = #lib.cloud_data.watchers[prop], 1, -1 do
+          local playerId = lib.cloud_data.watchers[prop][i]
+          
+          if DoesPlayerExist(playerId) then
+            table.insert(watchersList, playerId)
+          else
+            table.remove(lib.cloud_data.watchers[prop], i)
           end
-        end)
+        end
+        
+        lib.cloud_data.watchers[prop] = watchersList
+        
+        if #watchersList > 0 then
+          for _, playerId in ipairs(watchersList) do
+            TriggerClientEvent('ns_core.cloud-data:update', playerId, prop, newValue)
+          end
+        else
+          lib.cloud_data.watchers[prop] = nil
+        end
       end
     end
+  end
+
+  return getter, setter
 end
 
 --- @param prop string
+--- @return boolean exists
+function lib.cloud_data.exists(prop)
+  return lib.cloud_data.validProps[prop] == true
+end
+
+--- @param prop string
+--- @return any value
+function lib.cloud_data.getValue(prop)
+  if lib.cloud_data.validProps[prop] then
+    return lib.cloud_data.data[prop]
+  end
+  return nil
+end
+
+--- @param prop string
+--- @param value any
+--- @return boolean success
+function lib.cloud_data.setValue(prop, value)
+  if lib.cloud_data.validProps[prop] then
+    if lib.cloud_data.data[prop] ~= value then
+      lib.cloud_data.data[prop] = value
+
+      if lib.cloud_data.watchers[prop] ~= nil and #lib.cloud_data.watchers[prop] > 0 then
+        for _, playerId in ipairs(lib.cloud_data.watchers[prop]) do
+          TriggerClientEvent('ns_core.cloud-data:update', playerId, prop, value)
+        end
+      end
+    end
+    return true
+  end
+  return false
+end
+
 RegisterNetEvent('ns_core.cloud-data:watch', function(prop)
   local playerId = source
 
-  if playerId ~= nil and lib.cloud_data.data[prop] ~= nil then
-    if lib.cloud_data.watchers[prop] == nil then
-      lib.cloud_data.watchers[prop] = {}
-    end
+  if not playerId or not GetPlayerName(playerId) then
+    return
+  end
 
+  if not lib.cloud_data.validProps[prop] then
+    print(string.format("Warning: Client %d tried to watch non-existent property: %s", playerId, prop))
+    return
+  end
+
+  if lib.cloud_data.watchers[prop] == nil then
+    lib.cloud_data.watchers[prop] = {}
+  end
+
+  local alreadyWatching = false
+  for _, existingPlayerId in ipairs(lib.cloud_data.watchers[prop]) do
+    if existingPlayerId == playerId then
+      alreadyWatching = true
+      break
+    end
+  end
+
+  if not alreadyWatching then
     table.insert(lib.cloud_data.watchers[prop], playerId)
   end
+
+  TriggerClientEvent('ns_core.cloud-data:initial', playerId, prop, lib.cloud_data.data[prop])
 end)
+
+RegisterNetEvent('ns_core.cloud-data:unwatch', function(prop)
+  local playerId = source
+
+  if lib.cloud_data.watchers[prop] then
+    for i = #lib.cloud_data.watchers[prop], 1, -1 do
+      if lib.cloud_data.watchers[prop][i] == playerId then
+        table.remove(lib.cloud_data.watchers[prop], i)
+        break
+      end
+    end
+
+    if #lib.cloud_data.watchers[prop] == 0 then
+      lib.cloud_data.watchers[prop] = nil
+    end
+  end
+end)
+
+AddEventHandler('playerDropped', function(reason)
+  local playerId = source
+  
+  for prop, watchers in pairs(lib.cloud_data.watchers) do
+    for i = #watchers, 1, -1 do
+      if watchers[i] == playerId then
+        table.remove(watchers, i)
+      end
+    end
+
+    if #watchers == 0 then
+      lib.cloud_data.watchers[prop] = nil
+    end
+  end
+end)
+
+function lib.cloud_data.getStatus()
+  local status = {
+    totalProps = 0,
+    totalWatchers = 0,
+    propsWithWatchers = 0
+  }
+
+  for _ in pairs(lib.cloud_data.validProps) do
+    status.totalProps = status.totalProps + 1
+  end
+
+  for prop, watchers in pairs(lib.cloud_data.watchers) do
+    if watchers and #watchers > 0 then
+      status.propsWithWatchers = status.propsWithWatchers + 1
+      status.totalWatchers = status.totalWatchers + #watchers
+    end
+  end
+
+  return status
+end
